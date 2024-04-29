a64761c3dc67848877b9f685eb0f6459a038d73e
Dennis Miring'u <dennismiringu03@gmail.com>
2024-04-29 09:22:17 +0300

    some changes

--- sort.h
+++ sort.h
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size)
@@ -15,4 +31,4 @@ int partition(int *array, int low, int high, size_t size)
 void lomuto_quick_sort(int *array, int low, int high, size_t size)
 void quick_sort(int *array, size_t size)
 
-#end if
+#endif
