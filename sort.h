1c17d9eba601251ab01c003627e05d642152d1c8
Dennis Miring'u <dennismiringu03@gmail.com>
2024-04-29 09:31:10 +0300

    sort header

--- sort.h
+++ sort.h
@@ -22,13 +22,13 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size)
-listint_t *swap_node(listint_t *node, listint_t **list)
-void insertion_sort_list(listint_t **list)
-void selection_sort(int *array, size_t size)
-void swap(int *a, int *b)
-int partition(int *array, int low, int high, size_t size)
-void lomuto_quick_sort(int *array, int low, int high, size_t size)
-void quick_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int partition(int *array, int low, int high, size_t size);
+void lomuto_quick_sort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
